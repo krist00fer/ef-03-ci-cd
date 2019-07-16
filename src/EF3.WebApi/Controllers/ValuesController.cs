8e9f147e16cacd641233db6a5fe04e7a4f160e9e
Kristofer Liljeblad <kristol@microsoft.com>
2019-07-16 10:11:11 +0200

    Fixed bug in test

--- src/EF3.WebApi/Controllers/ValuesController.cs
+++ src/EF3.WebApi/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace EF3.WebApi.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "hello", "world" };
+            return new string[] { "Hello", "CI/CD" };
         }
 
         // GET api/values/5
